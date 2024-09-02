04fb2a64ce292485ae726aa3e23c8f2db385eb42
AndresA <afanadoraf@hotmail.com>
2024-09-01 20:43:40 -0500

    Taller Rabbit SpringBoot

---
+++ src/test/java/taller_rabbit_springboot/TallerRabbitSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package taller_rabbit_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerRabbitSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
